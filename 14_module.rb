1604d019db1e39f9c39c06adb4c6e371c88edfd3
Mirzagayev Aydin <am180@hdm-stuttgart.de>
2019-12-09 20:54:53 +0100

    Skripte aktualisieren

--- 14_module.rb
+++ 14_module.rb
@@ -1,14 +1,24 @@
-require './12_klassen_und_instanzen'
-
-class Student < Person
-	attr_reader :semester
-	attr_writer :semester
-
-	def statusinfo
-		"Status: #{@status}"
+module TestModule1
+	def fnc1
+		"Testmodule 1"
+	end
+end
+module TestModule2
+	def fnc2
+		"Testmodule 2"
 	end
 end
 
-teststudent = Student.new("Mustermann", "Max", "Student", "mm123")
+class Testklasse
+	attr_reader :name
+	include TestModule1
+	include TestModule2
 
-puts teststudent.statusinfo
+	def initialize(name)
+		@name = name
+	end
+end
+testklasseninstanz = Testklasse.new("Max Mustermann")
+
+puts testklasseninstanz.name
+puts testklasseninstanz.fnc1, testklasseninstanz.fnc2
